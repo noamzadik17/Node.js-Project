48166e8b371dfce0a88cc33839c0bee442f746a5
noamzadik17 <124884490+noamzadik17@users.noreply.github.com>
2023-03-05 10:37:33 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.get('*', function (req, res) {
+    res.status(404).sendFile(path.join(__dirname, "public", "404Page"))
+});
 routesInit(app);
 const server = http.createServer(app);
 let port = process.env.PORT || 3001;
