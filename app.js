f9f9ed8da766b43b476d6c3b74bda8cbedbf9bc2
noamzadik17 <124884490+noamzadik17@users.noreply.github.com>
2023-03-05 10:45:09 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-app.all('*', (req, res) => {
-    res.status(404).send('<h1>404! Page not found</h1>');
-});
 routesInit(app);
+app.use((req, res) => {
+    res.status(404).send('<h1>404 Page Was Not Found!</h1>');
+});
 const server = http.createServer(app);
 let port = process.env.PORT || 3001;
 server.listen(port);
-
